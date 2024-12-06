c8d1293af4c33226e882c2e2858e356eef289fa9
vitash <hwj.0_0@foxmail.com>
2024-12-06 14:50:20 +0800

    init

---
+++ AvaCs3/App.axaml.cs
@@ -0,0 +1,36 @@
+﻿using AvaCs3.ViewModels;
+using AvaCs3.Views;
+
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+
+namespace AvaCs3;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow
+            {
+                DataContext = new MainViewModel()
+            };
+        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        {
+            singleViewPlatform.MainView = new MainView
+            {
+                DataContext = new MainViewModel()
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ AvaCs3/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AvaCs3.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
